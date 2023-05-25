af6785d99fc0cda4aa38ddbd9b3f2c7c803bafd7
pretendtobefree <105958606+pretendtobefree@users.noreply.github.com>
2023-05-25 12:52:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/login/login -f root/g' package/base-files/files/etc/config/ttyd
+#sed -i 's/login/login -f root/g' package/base-files/files/etc/config/ttyd
